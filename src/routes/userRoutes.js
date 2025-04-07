ede88fcd7942de73036f2fddd72b2a72fc719484
Gabriel Torres <gbrtorress@gmail.com>
2025-04-07 09:00:59 -0300

    changing some errors, project finalized

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
-const express = rquire('express');
+const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/users', userController.createUser);
 router.get('/users', userController.getAllUsers);
-router.delete('/users:id', userController.deleteUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
