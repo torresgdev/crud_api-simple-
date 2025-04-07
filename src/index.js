ab21365413caa0881944b11e7d2d7effb649d0cc
Gabriel Torres <gbrtorress@gmail.com>
2025-04-07 09:41:01 -0300

    added cors for better visualization in frontend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', userRoutes);
 
 app.listen(3000, () => {
